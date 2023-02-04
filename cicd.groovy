5a151ea70370e2ba2abc40b3d70d52f1bd45b77e
Mike Fulton <mikefultonpersonal@gmail.com>
2023-02-03 22:52:16 -0500

    rename to libpipeline from pipeline

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux')
 {
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/pipelineport.git'), string(name: 'PORT_DESCRIPTION', value: 'libpipeline is a C library for setting up and running pipelines of processes, without needing to involve shell command-line parsing which is often error-prone and insecure' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libpipelineport.git'), string(name: 'PORT_DESCRIPTION', value: 'libpipeline is a C library for setting up and running pipelines of processes, without needing to involve shell command-line parsing which is often error-prone and insecure' )]
   }
 }
