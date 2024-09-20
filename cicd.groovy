d66dfecce183ac967af560432d2f853e367bcea6
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:59 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/libpipelineport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/libpipelineport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libpipelineport.git'), string(name: 'PORT_DESCRIPTION', value: 'libpipeline is a C library for setting up and running pipelines of processes, without needing to involve shell command-line parsing which is often error-prone and insecure' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libpipelineport.git'), string(name: 'PORT_DESCRIPTION', value: 'libpipeline is a C library for setting up and running pipelines of processes, without needing to involve shell command-line parsing which is often error-prone and insecure' )]
   }
 }
